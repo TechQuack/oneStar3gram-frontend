0a532aa83b74db839b0ec542c36f600a5882b49b
Erwann <montagne.erwann.2@gmail.com>
2024-12-19 20:55:05 +0100

    feat : create profile page to show user info

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from 'keycloak-angular';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent implements OnInit{
+
+  username : string | undefined = "";
+  email : string | undefined = "";
+  firstName : string | undefined = "";
+  lastName : string | undefined = "";
+
+  constructor(private  keycloakService : KeycloakService) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    let user = await this.keycloakService.loadUserProfile(false);
+    this.username = user.username;
+    this.email = user.email;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+  }
+
+}
