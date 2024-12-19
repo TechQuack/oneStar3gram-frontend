53052a301ad455f5b3ce7df3789d4be781834058
ellexart <axelle.mazuy@gmail.com>
2024-12-19 21:50:33 +0100

    feat: show post media and information

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PostService } from '../services/post.service';
 import { Post } from '../entities/post.entity';
 
@@ -11,13 +11,15 @@ import { Post } from '../entities/post.entity';
 })
 export class PostComponent {
 
+  @Input() id: number = 0;
+
   post: Post | null = null;
 
   constructor(private postService: PostService) {}
 
 
   async ngOnInit() {
-    this.postService.getPost(1).subscribe(post => this.post = post)
+    this.postService.getPost(this.id).subscribe(post => this.post = post)
   }
 
 }
