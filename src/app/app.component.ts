21db6d2c7733798bcd03abed5098e8a8109b37c7
izana <izana.khabouri@univ-rouen.fr>
2024-12-20 10:49:14 +0100

    feat: link data to nav bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { KeycloakBearerInterceptor } from 'keycloak-angular';
 import { KeycloakProfile } from './../../node_modules/keycloak-js/lib/keycloak.d';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,8 @@ export class AppComponent {
   title = 'OneStar3gram-front';
   public isLogged = false;
   public user : KeycloakProfile | null = null;
+
+  loadProfile() {
+    alert("mon profil a été cliqué")
+  }
 }
