642c0ddac82fa639929427788a9398e680068a34
Hortense Cauchois <hortensecauchois@gmail.com>
2024-12-23 15:58:42 +0100

    feat: add only admin accessible delete button on post component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { PostService } from '../services/post.service';
 import { Post } from '../entities/post.entity';
+import {KeycloakService} from 'keycloak-angular';
+import {KeycloakProfile} from 'keycloak-js';
 
 @Component({
   selector: 'app-post',
@@ -12,14 +14,22 @@ import { Post } from '../entities/post.entity';
 export class PostComponent {
 
   @Input() id: number = 0;
+  isAdmin: boolean = false;
 
   post: Post | null = null;
 
-  constructor(private postService: PostService) {}
+  constructor(private postService: PostService, private keycloackService: KeycloakService) {}
 
 
   async ngOnInit() {
-    this.postService.getPost(this.id).subscribe(post => this.post = post)
+    this.postService.getPost(this.id).subscribe(post => this.post = post);
+    this.isAdmin = this.keycloackService.getUserRoles().includes("Admin");
+  }
+
+  deletePost() {
+    if (this.isAdmin) {
+      this.postService.deletePost(this.id);
+    }
   }
 
 }
