7b88ec89d541164917aebd9b659f896ff8c38129
Hortense Cauchois <hortensecauchois@gmail.com>
2024-12-23 14:44:30 +0100

    feat: add route for addContent component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {AddContentComponent} from './add-content/add-content.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'add', component: AddContentComponent},
 ];
