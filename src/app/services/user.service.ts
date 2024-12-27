48c3f6db5627971b8a6a73fa8c4ecd12cacde0ef
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-12-27 12:08:14 +0100

    feat: default value is the role of the user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,7 +3,7 @@ import { KeycloakService } from 'keycloak-angular';
 
 import {User} from '../entities/user.entity';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {forkJoin, Observable} from 'rxjs';
+import {forkJoin, map, Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 
 @Injectable({
@@ -23,11 +23,11 @@ export class UserService {
     return this.http.get<User[]>(environment.keycloakUrl + 'users', {headers});
   }
 
-  public hasRole(role: string): boolean {
+  hasRole(role: string): boolean {
       return this.keycloakService.isUserInRole(role)
   }
 
-  public changeRole(userId: number, role: string): Observable<any> {
+  changeRole(userId: number, role: string): Observable<object> {
     this.removeRoles(userId, role)
     return new Observable(observer => {
       const token = this.keycloakService.getToken();
@@ -39,7 +39,7 @@ export class UserService {
         + userId + '/role-mappings/realm'
       if (role !== 'None') {
         this.getRoleByName(role).subscribe({
-          next: (newRole: any) => {
+          next: (newRole: object) => {
             const rolePayload = [newRole];
             this.http.post(roleUrl, rolePayload, {headers}).subscribe({
               next: response => {
@@ -53,10 +53,31 @@ export class UserService {
     });
   }
 
+  getUserRole(userId: number): Observable<string> {
+      const token = this.keycloakService.getToken();
+      const headers = new HttpHeaders({
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      });
+
+      const roleUrl = environment.keycloakUrl + 'users/'
+        + userId + '/role-mappings/realm'
+      return this.http.get<any[]>(roleUrl, { headers }).pipe(
+        map(roles => {
+          const roleNames = roles.map(role => role.name)
+          if (roleNames.includes('Admin')) {
+            return 'Admin'
+          } else if (roleNames.includes('Privileged')) {
+            return 'Privileged'
+          } else {
+            return 'None'
+          }
+        })
+      )
+  }
 
 
-
-  private getRoleByName(roleName: string): Observable<any> {
+  private getRoleByName(roleName: string): Observable<object> {
     const url = environment.keycloakUrl + 'roles/' + roleName;
     const token = this.keycloakService.getToken();
     const headers = new HttpHeaders({
@@ -66,7 +87,7 @@ export class UserService {
     return this.http.get(url, { headers });
   }
 
-  private removeRoles(userId: number, role: string) {
+  private removeRoles(userId: number, role: string): void {
     const rolesToRemoveNames = this.getRolesToRemove(role)
     const rolesToRemove = forkJoin(rolesToRemoveNames.map(role => this.getRoleByName(role)))
     rolesToRemove.subscribe({
@@ -84,7 +105,7 @@ export class UserService {
     })
   }
 
-  private getRolesToRemove(currentRole: string): any[] {
+  private getRolesToRemove(currentRole: string): string[] {
     switch (currentRole) {
       case 'Admin':
         return ['Privileged'];
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 import {User} from '../entities/user.entity';
 import {UserService} from '../services/user.service';
+import {FormsModule} from '@angular/forms';
+import {forkJoin, map} from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
   users: User[] = []
-
   constructor(private userService: UserService) {
   }
 
   async ngOnInit() {
-    this.userService.getUsers().subscribe(users => {
-      this.users = users
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        const roleRequests = users.map((user) =>
+          this.userService.getUserRole(user.id).pipe(
+            map((role) =>
+              ({...user, role}))
+          )
+        );
+        forkJoin(roleRequests).subscribe({
+          next: (usersWithRoles: User[]) => {
+            this.users = usersWithRoles;
+          },
+        });
+      },
     })
   }
 
