a348e665c952e54ae75ff84e78d0714fb8dab860
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-12-30 10:22:51 +0100

    feat: redirect user to home page after logging out

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     apiUrl: 'https://proxy-onestar3gram:8081/',
-    keycloakUrl: 'https://proxy-onestar3gram:8080/admin/realms/OneStar3gram/'
+    keycloakUrl: 'https://proxy-onestar3gram:8080/admin/realms/OneStar3gram/',
+    homePage: 'https://proxy-onestar3gram'
 };
